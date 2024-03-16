9a598cfb781d88b4cdf8d551566d31b4ea1f04c5
Matthew McGovern <mattmcg37@gmail.com>
2024-03-16 00:16:49 -0300

    data in console, but not on page

--- crypto2/src/App.jsx
+++ crypto2/src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Coins from './components/Coins';
+
 
 
 function App() {
@@ -20,7 +22,7 @@ function App() {
 
   return (
     <>
-      heyyy
+      <Coins coins={coins} />
     </>
   )
 }
