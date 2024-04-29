04379bfe7c91f774968901a364534bb7a62d7e6c
Matthew McGovern <mattmcg37@gmail.com>
2024-04-29 08:59:24 -0300

    navbar complete

--- MMportfolio/src/components/Navbar.jsx
+++ MMportfolio/src/components/Navbar.jsx
@@ -38,17 +38,32 @@ const Navbar = () => {
           ))}
         </ul>
 
-        <di className="sm:hidden flex flex-1 justify-end items-center">
+        <div className="sm:hidden flex flex-1 justify-end items-center">
           <img src={toggle ? close : menu}
               alt="menu"
               className='w-[28px] h-[28px] object-contain cursor-pointer'
               onClick={() => setToggle(!toggle)}
           />
-        </di>
 
+          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx04 my-2 min-w-[140px] z-10 rounded-xl`}>
+          <ul className='list-none flex justify-end items-start flex-col gap-4'>
+          {navLinks.map((Link) => (
+            <li
+            key={Link.id}
+            className={`${active === Link.title ? "text-white" : "text-secondary"} font-poppins font-medium cursor-pointer text-[16px]`}
+            onClick={() => {
+              setToggle(!toggle);
+              setActive(Link.title);
+            }}
+          >
+            <a href={`#${Link.id}`}>{Link.title}</a>
+          </li>
+          ))}
+        </ul>
+          </div>
+
+        </div>
       </div>
-
-
     </nav>
   )
 }
