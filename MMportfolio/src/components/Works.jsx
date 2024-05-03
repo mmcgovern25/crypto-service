7f33239ea8aa2ab44e0314cd10908e1da24e9d11
Matthew McGovern <mattmcg37@gmail.com>
2024-05-03 08:05:36 -0300

    projects part 1

--- MMportfolio/src/components/Works.jsx
+++ MMportfolio/src/components/Works.jsx
@@ -1,9 +1,60 @@
-
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { github } from '../assets/';
 import { SectionWrapper } from '../hoc';
+import { projects } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
+
+const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
+  return (
+    <motion.div variants={fadeIn('up', 'spring', index * 0.5, 0.75)}>
+      <div
+
+        className='bg-tertiary rounded-2xl p-5 2xl sm:w-[360px] w-full'
+      >
+
+        <div className='relative w-full h-[230px]'>
+          <img
+            src={image}
+            alt={name}
+            className='w-full h-full object-cover rounded-2xl'
+          />
+
+          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+            <div
+              onClick={() => window.open(source_code_link, '_blank')}
+              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+            >
+              <img
+                src={github}
+                alt='github'
+                className='w-1/2 h-1/2 object-contain'
+              />
+
+            </div>
+            <div
+              onClick={() => window.open(source_code_link, '_blank')}
+              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+            >
+              <img
+                src={github}
+                alt='github'
+                className='w-1/2 h-1/2 object-contain'
+              />
+
+            </div>
+          </div>
+        </div>
+
+        <div className='mt-5'>
+          <h3>{name}</h3>
+          <p>{description}</p>
+        </div>
+
+      </div>
+    </motion.div>
+  )
+}
 
 const Works = () => {
   return (
@@ -11,8 +62,8 @@ const Works = () => {
       <motion.div
         variants={textVariant()}
       >
-      <p className={styles.sectionSubText}>My work</p>
-          <h2 className={styles.sectionHeadText}>Projects.</h2>
+        <p className={styles.sectionSubText}>My work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
       </motion.div>
 
       <div className='w-full flex'>
@@ -20,15 +71,22 @@ const Works = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
-          Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          Following projects showcase my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos in it. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.
         </motion.p>
       </div>
+
+      <div className='mt-2 flex flex-wrap gap-7'>
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={`project-${index}`}
+            index={index}
+            {...project}
+          />
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
+
 
 export default SectionWrapper(Works, "");
