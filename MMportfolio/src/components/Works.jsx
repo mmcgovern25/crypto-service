345538a99f060e86f76ef33c073cb4c17ff4fd9c
Matthew McGovern <mattmcg37@gmail.com>
2024-05-22 12:17:06 -0300

    works now visible on mobile!

--- MMportfolio/src/components/Works.jsx
+++ MMportfolio/src/components/Works.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { github, play } from '../assets/';
@@ -9,6 +10,9 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link, 
   return (
     <motion.div
       variants={fadeIn('up', 'spring', index * 0.5, 0.75)}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: true, amount: 0.1 }}
       className='bg-tertiary rounded-2xl p-5 sm:w-[340px] w-full'
     >
       <div className='relative w-full h-[230px]'>
@@ -58,16 +62,19 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link, 
 const Works = () => {
   return (
     <>
-      <motion.div
-        variants={textVariant()}
-      >
-        <p className={styles.sectionSubText}>My work</p>
-        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      <motion.div initial="hidden"
+          whileInView="show"
+          viewport={{ once: true, amount: 0.1 }} variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>My work</p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>Projects.</h2>
       </motion.div>
 
       <div className='w-full flex'>
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: true, amount: 0.1 }}
           className='mt-3 mb-10 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
           The six projects below I felt best showcased my abilities as a evergrowing developer. I made it a priority to work on projects that interested me on a personal level. So through these projects, I began to felt like an artist freely expressing myself as well. A few of these projects specifically, such as Connect & Clean, and Alec Simonvitch CPA were built with the attend of gaining real web traction as legitmate businesses in the near future.
