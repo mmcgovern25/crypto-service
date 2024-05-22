77487bc536f759a5ea6e65e2c5b89d2246f33b6c
Matthew McGovern <mattmcg37@gmail.com>
2024-05-22 11:54:06 -0300

    tech border added and motioN

--- MMportfolio/src/components/Technologies.jsx
+++ MMportfolio/src/components/Technologies.jsx
@@ -5,8 +5,9 @@ import { SectionWrapper } from "../hoc";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { textVariant } from "../utils/motion";
+import { fadeIn } from "../utils/motion";
 
- const Technologies = () => {
+ const Technologies = ({ index }) => {
   return (
     <>
     <motion.div variants={textVariant()}>
@@ -19,16 +20,23 @@ import { textVariant } from "../utils/motion";
       </motion.div>
 
       <div className='flex flex-row flex-wrap justify-center gap-12 mt-24'>
-  {technologies.map((technology, index) => (
-    <Tilt key={index} className='tech-icon-container green-pink-gradient p-[1px] rounded-[12px]'>
-      <img
-        src={technology.icon}
-        alt={technology.name}
-        className='tech-icon w-28 h-28 rounded-[12px] bg-tertiary shadow-card'
-      />
-    </Tilt>
-  ))}
-</div>
+      {technologies.map((technology, index) => (
+        <Tilt key={index} className='p-[1px] rounded-[12px]'>
+          <motion.div
+            variants={fadeIn('right', 'spring', index * 0.2, 0.5)}
+            className='green-pink-gradient p-[1px] rounded-[12px]'
+          >
+            <div className='bg-tertiary rounded-[12px]'>
+              <img
+                src={technology.icon}
+                alt={technology.name}
+                className='w-28 h-28 rounded-[12px] object-contain'
+              />
+            </div>
+          </motion.div>
+        </Tilt>
+      ))}
+    </div>
     </>
   )
 }
